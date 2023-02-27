4525a1ac2a98458a602714d4dbd5d7a351c3de25
geronimo <geronimo@geronimos-MacBook-Pro.local>
2023-02-26 17:38:55 -0800

    updates

--- ecli.py
+++ ecli.py
@@ -1,4 +1,49 @@
 import click
+import json
+import requests
+import datetime
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+# Temporary solution to set URL
+uservar = 2
+uservar = int(uservar)
+if uservar == 1:
+    userapiurl = "https://api.emlalock.com/info?userid=s9n33u90x27jqsr&apikey=1intd05gi0l"
+    useruuid = "addthislater"
+elif uservar == 2:
+    userapiurl = "https://api.emlalock.com/info?userid=4d89h9ev72jmft8&apikey=wh2wqiikwn"
+    useruuid = "4d89h9ev72jmft8"
+
+# Set URL based on selected user
+url = requests.get(userapiurl)
+text = url.text
+#sessionDict = json.loads(text)
+
+#print(sessionDict)
+
+def get_holder_info():
+    global holder_id
+    sessionDict = json.loads(text)
+    holder_id = sessionDict['chastitysession']['holderid']
+    #print(holder_id)
+
+def get_time_stats():
+    global startDate
+    global startDateFriendly
+    global endDate
+    global endDateFriendly
+    global timeInLock
+    global timeInLockFriendly
+    sessionDict = json.loads(text)
+    startDate = sessionDict['chastitysession']['startdate']
+    startDateFriendly = datetime.datetime.fromtimestamp(startDate)
+    endDate = sessionDict['chastitysession']['enddate']
+    endDateFriendly = datetime.datetime.fromtimestamp(endDate)
+    timeInLock = endDate - startDate
+    timeInLockFriendly = datetime.datetime.fromtimestamp(timeInLock)
 
 @click.group()
 def cli():
@@ -23,6 +68,36 @@ def showuser(username):
         help ='This is a greeting')
 def hello(string):
     click.echo("Hello, {}".format(string))
+
+@cli.command()
+@click.option('--show_holder', required=False, default ='World',
+        help ='Show existing holder if any')
+def show_holder(show_holder):
+    #starting_holder_id = sessionDict['chastitysession']['holderid']
+    get_holder_info()
+    if holder_id == useruuid:
+        click.echo("You do not have a keyholder.".format(show_holder))
+    else:
+        # Need to fix this
+        #click.echo(f"Holder ID is: {holder_id}".format(show_time))
+        click.echo(f"Holder ID is: {holder_id}".format(show_holder))
+
+@cli.command()
+@click.option('--show_time', required=False, default ='World',
+        help ='Show existing holder if any')
+def show_time(show_time):
+    #starting_holder_id = sessionDict['chastitysession']['holderid']
+    get_time_stats()
+    click.echo(f"Session Start: {startDateFriendly}".format(show_time))
+    click.echo(f"Session End:   {endDateFriendly}".format(show_time))
+    click.echo(f"Time Locked:   {timeInLockFriendly}".format(show_time))
+    #table = Table(show_header=True, header_style="blue")
+    #table.add_column("Start Date")
+    #table.add_column("End Date")
+    #table.add_row(startDateFriendly)
+    #table.add_row(endDateFriendly)
+    #console.print(table)
+
 
 if __name__=="__main__":
     cli()
